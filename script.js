b113e0bb2f7b58b23cfb575bf8816312770e04bc
Nurcholis Al Fachri <nurcholis.alfachri@gmail.com>
2023-06-01 14:49:16 +0700

    menambah semua file

---
+++ script.js
@@ -0,0 +1,139 @@
+
+// /*==================== SHOW MENU ====================*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+            // class show-menu berisi class nav__menu
+        })
+    }
+}
+
+showMenu('nav-toggle','nav-menu')
+// menambahkan class show menu ketika burger menu d klik di mobile
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+// menghapus/menghilangkan show menu ketika li di klik
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sectionz = document.querySelectorAll('section[id]')
+// console.log(sectionz); id dr tiap2 section
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sectionz.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive);
+// merubah warna li ketika halaman ada d halaman li
+
+
+/*==================== ADD HEADER SHADOW ====================*/
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 200) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+// ketika d scroll ke bawah >= 200, akan menambah class scroll header yg berisi shadow
+
+
+/*==================== SHOW SCROLL TOP ====================*/
+function scrollTop(){
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if(this.scrollY >= 560) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
+// ketika di scroll ke bawah >= 560, akan memunculkan icon u naik ke atas
+
+
+/*==================== DARK MODE ====================*/
+// Dark Light Theme
+const themeButton = document.getElementById('theme-button')
+const sections = document.querySelectorAll('.section')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bx-sun'
+// variable buat dijadikan nilai true or false
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => sections.forEach(function(sec) {
+    sec.classList.contains(darkTheme) ? 'dark' : 'light'
+}) 
+const getCurTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bx-moon' : 'bx-sun'
+// klo ada class bx-sun, yg d ambil adalah bx-moon
+
+// We validate if the user previously choose a topic
+if(selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    sections.forEach(function(sec) {
+        sec.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    })
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    // kalau body tema nya dark, tambah class dark-theme
+    themeButton.classList[selectedIcon === 'bx-moon' ? 'add' : 'remove'](iconTheme)
+    // menambah class bx-sun (iconTheme)
+}
+// validasi
+
+// Active / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    sections.forEach(function(sec) {
+        sec.classList.toggle(darkTheme)
+    })
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user choose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-theme', getCurTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+// Scroll Reveal Animation
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 2000,
+    reset: true
+});
+
+sr.reveal(` .home__data, .hero__container,
+            .about__data, .about__socmed,
+            .services__content, .services__container,
+            .thumbnail-container, 
+            .portofolio__container,
+            .footer__content, .footer__copy`, {
+    interval: 200
+})
+
+
